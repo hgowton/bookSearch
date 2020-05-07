7f77a130c938104223dfa2ca96e021de492270f2
hgowton <hgowton@gmail.com>
2020-05-06 20:50:01 -0400

    API search as for loop

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -9,7 +9,7 @@ import {Input, FormBtn} from "../components/Form";
 
 class Search extends Component {
     state={
-        results: {},
+        books: [],
         search: ""
     };
 
@@ -19,7 +19,20 @@ class Search extends Component {
 
     searchBooks = query => {
         API.search(query)
-        .then(res => this.setState({ result: res.data}))
+        .then(res => {
+            const booksArray = []
+            for (var i=0; i < res.data.items.length; i++) {
+                booksArray.push(
+                    {id: res.data.items[i].volumeInfo.title,
+                        title: res.data.items[i].volumeInfo.description,
+                        authors: res.data.items[i].volumeInfo.authors,
+                        image: res.data.items[i].volumeInfo.imageLinks.thumbnail,
+                        link: res.data.items[i].volumeInfo.infoLink
+                    })
+                }
+                this.setState({ books: booksArray, search : ""});
+                console.log(this.state.books)
+            })
         .catch(err => console.log(err));
     };
 
@@ -60,8 +73,8 @@ class Search extends Component {
                     {this.state.books.map(book => (
                         <Row>
                         <Col size="md-12">
-                            {this.state.results.items[i].volumeInfo.title}
-                            <ViewBtn onClick={(this.state.results.items[i].volumeInfo.infoLink) => console.log}/>
+                            {this.state.results.items[book].volumeInfo.title}
+                            <ViewBtn onClick={(this.state.results.items[i].volumeInfo.infoLink)}/>
                         </Col>
                     </Row>
                     <Row>
@@ -69,7 +82,7 @@ class Search extends Component {
                             {this.state.results.items[i].volumeInfo.author}
                             <SaveBtn onClick={() => this.saveBook({
                                 title: this.state.results.items[i].volumeInfo.title,
-                                author: this.state.results.items[i].volumeInfo.author[0],
+                                author: [this.state.results.items[i].volumeInfo.author[0]],
                                 synopsis: this.state.results.items[i].volumeInfo.description,
                                 image: this.state.results.items[i].volumeInfo.imageLink.thumbnail,
                                 link: this.state.results.items[i].volumeInfo.infoLink
